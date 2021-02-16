502f07ad2f6f0fb503ef9903aec2b1986d2fb0cb
JC <jctrilamanila@gmail.com>
2021-02-15 20:54:45 -0500

    Added firebase API

---
+++ amazon/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAJn0e9cVRbIGndFnurPyCCA6M8NH7h1hM",
+  authDomain: "fir-ad8ea.firebaseapp.com",
+  projectId: "fir-ad8ea",
+  storageBucket: "fir-ad8ea.appspot.com",
+  messagingSenderId: "1002926262010",
+  appId: "1:1002926262010:web:b2cd0319314be2cc7feb43",
+  measurementId: "G-E4G6VZS050"
+};
