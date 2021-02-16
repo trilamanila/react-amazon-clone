b649148d4c8a356c32ab3a6c3696e81901cc5d1e
JC <jctrilamanila@gmail.com>
2021-02-16 01:46:25 -0500

    added and styled home banner background

--- amazon/src/Home.js
+++ amazon/src/Home.js
@@ -2,7 +2,17 @@ import React from "react";
 import "./Home.css";
 
 function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
