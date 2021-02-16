ad62194c8294c0f661c84f5836b44a7adf5b5cb9
JC <jctrilamanila@gmail.com>
2021-02-16 04:44:17 -0500

    added react-router-dom

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -9,13 +9,13 @@ function App() {
   return (
     <Router>
       <div className="app">
+        <Header />
+
         <Switch>
           <Route path="/checkout">
-            <Header />
             <Checkout />
           </Route>
           <Route path="/">
-            <Header />
             <Home />
           </Route>
         </Switch>
