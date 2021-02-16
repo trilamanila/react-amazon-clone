9bd7f3880f2401f745bbd70da15fbeae9e49672f
JC <jctrilamanila@gmail.com>
2021-02-16 15:14:03 -0500

    created reducer state management to make add to cart button functional

--- amazon/src/Home.js
+++ amazon/src/Home.js
@@ -14,6 +14,7 @@ function Home() {
 
         <div className="home__row">
           <Product
+            id={111111}
             title="The Bitcoin Standard"
             price={19.99}
             image="https://image.slidesharecdn.com/1119473861-thebitcoinstandardbysaifedeanammous-181029155741/95/the-bitcoin-standard-pdf-saifedean-ammous-the-decentralized-alternative-to-central-banking-1-638.jpg?cb=1540828701"
@@ -21,6 +22,7 @@ function Home() {
             rating={5}
           />
           <Product
+            id={111112}
             title="AquaSonic Black Series Ultra Whitening Toothbrush – ADA Accepted Rechargeable Toothbrush - 8 Brush Heads & Travel Case - Ultra Sonic Motor & Wireless Charging - 4 Modes w Smart Timer - Sonic Electric"
             price={139.99}
             image="https://images-na.ssl-images-amazon.com/images/I/71byv51vxAL._SX425_.jpg"
@@ -31,6 +33,7 @@ function Home() {
 
         <div className="home__row">
           <Product
+            id={111113}
             title="CASEKOO Defender Designed for iPhone 12 Pro Max Case, [Military Grade Drop Protection], Clear Shockproof Protective Phone Case Slim Thin Cover 5G 6.7 inch 2020- Black "
             price={23.99}
             image="https://images-na.ssl-images-amazon.com/images/I/81FigHXuPHL._AC_SX679_.jpg"
@@ -38,6 +41,7 @@ function Home() {
             rating={4}
           />
           <Product
+            id={111114}
             title="32.8ft Led Lights dalattin Led Strip Lights Color Changing Lights with 44 Keys Remote,1 Roll of 32.8ft"
             price={22.99}
             image="https://images-na.ssl-images-amazon.com/images/I/81Pzl06kXPL._AC_SX425_.jpg"
@@ -45,6 +49,7 @@ function Home() {
             rating={5}
           />
           <Product
+            id={111115}
             title=" Huanuo Dual Monitor Stand - Adjustable Spring Monitor Desk Mount Swivel Vesa Bracket with C Clamp, Grommet Mounting Base for 17 to 27 Inch Computer Screens - Each Arm Holds 4.4 to 14.3lbs "
             price={52.99}
             image="https://images-na.ssl-images-amazon.com/images/I/71Aq-5A7yFL._AC_SX425_.jpg"
@@ -55,6 +60,7 @@ function Home() {
 
         <div className="home__row">
           <Product
+            id={111116}
             title="Novogratz Brittany Sofa Futon - Premium Upholstery and Wooden Legs - Navy Blue "
             price={389.99}
             image="https://images-na.ssl-images-amazon.com/images/I/81KZlom2DHL._AC_SX679_.jpg"
