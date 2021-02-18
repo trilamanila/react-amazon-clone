8f44d37d47311e1dbd57236db5d1238c2c750868
JC <jctrilamanila@gmail.com>
2021-02-18 14:51:15 -0500

    made sign out in header responsive

--- amazon/src/Header.js
+++ amazon/src/Header.js
@@ -4,10 +4,16 @@ import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
+import { auth } from "./firebase";
 
 function Header() {
   const [{ basket, user }, dispatch] = useStateValue();
 
+  const handleAuthenticaton = () => {
+    if (user) {
+      auth.signOut();
+    }
+  };
   return (
     <div className="header">
       <Link to="/">
@@ -23,8 +29,8 @@ function Header() {
       </div>
 
       <div className="header__nav">
-        <Link to="/login">
-          <div className="header__option">
+        <Link to={!user && "/login"}>
+          <div onClick={handleAuthenticaton} className="header__option">
             <span className="header__optionLineOne">Hello Guest</span>
             <span className="header__optionLineTwo">
               {user ? "Sign Out" : "Sign In"}
