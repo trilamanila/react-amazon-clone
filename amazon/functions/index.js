76036c65712236fcb2a9e47d613f9abda29e75ff
JC <jctrilamanila@gmail.com>
2021-02-21 09:59:12 -0500

    deployed live website

--- amazon/functions/index.js
+++ amazon/functions/index.js
@@ -5,31 +5,38 @@ const stripe = require("stripe")(
   "sk_test_51IMoNLHUqOCDm6hBF7Cfbj9tF7s6lg1RpbmvyNZp7dTegVkG65yA3SWPrMWZoiwCAuhuyrSovjP3HhH4ZL7XxzCI00OPTOyKRZ"
 );
 
-//API
+// API
 
-//App config
+// - App config
 const app = express();
-//Middleware
+
+// - Middlewares
 app.use(cors({ origin: true }));
 app.use(express.json());
 
-//API routes
+// - API routes
 app.get("/", (request, response) => response.status(200).send("hello world"));
 
 app.post("/payments/create", async (request, response) => {
   const total = request.query.total;
 
-  console.log("Payment Request Recieved !!! for this amount >>> ", total);
+  console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
 
   const paymentIntent = await stripe.paymentIntents.create({
     amount: total, // subunits of the currency
     currency: "usd"
   });
 
-  //Ok Created
+  // OK - Created
   response.status(201).send({
     clientSecret: paymentIntent.client_secret
   });
 });
-//Listen command
+
+// - Listen command
 exports.api = functions.https.onRequest(app);
+
+// Example endpoint
+// http://localhost:5001/challenge-4b2b2/us-central1/api
+
+//
--- amazon/src/axios.js
+++ amazon/src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/fir-ad8ea/us-central1/api"
+  baseURL: "https://us-central1-fir-ad8ea.cloudfunctions.net/api"
+
+  //"http://localhost:5001/fir-ad8ea/us-central1/api"
 });
 
 export default instance;
